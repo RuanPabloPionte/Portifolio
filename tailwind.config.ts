db149ee5ae4092cb403cb20640f323033664a4ac
Ruan Pablo Pionte <ruanpablopionte@gmail.com>
2023-10-14 16:05:10 -0300

    implement myTheme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,33 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+  daisyui: {
+    themes: [
+      {
+        light: {
+          primary: "#e8e9e9",
+          secondary: "#CCDEF3",
+          info: "#8FC2FD",
+          accent: "#3E7992",
+          neutral: "#3d4451",
+          "base-100": "#ffffff",
+        },
       },
-    },
+      "dark",
+    ],
   },
-  plugins: [],
-}
-export default config
+
+  plugins: [require("daisyui")],
+};
+export default config;
+
+// #e8e9e9 braanco do fundo
+// 8FC2FD azul navbar
+// 6796CD azul texto
+// CCDEF3 azul bg do card
+// 3E7992 azul do fundo da tecnologias
